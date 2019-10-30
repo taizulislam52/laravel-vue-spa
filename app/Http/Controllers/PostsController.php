ae41ff09cbcde81eb54d8780ce9ac88c48137bd0
Taizul52 <islam.taijul133052@gmail.com>
2019-10-30 17:18:40 +0600

    create rest api for posts

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PostResource::collection(\App\Post::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return new PostResource(\App\Post::create($this->validatePost()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+       return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        return new PostResource($post->update($this->validatePost()));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+       $post->delete();
+       return new PostResource($post);
+    }
+
+    public function validatePost(){
+        return request()->validate([
+            'title'=>'required',
+            'body'=>'required',
+        ]);
+    }
+}
